77cc563d23d8449c0257215d7fd35492e1e08e54
calamari <g.tavonius@gmail.com>
2013-01-08 19:06:45 +0100

    Allow List of friends for ShootOnSight Behavior for ships to not shoot

--- assets/javascripts/ai/behavior.js
+++ assets/javascripts/ai/behavior.js
@@ -3,30 +3,43 @@
 ;(function(win) {
   "use strict";
 
+  /**
+   * Baseclass for any behavior
+   * @param {Object} [config] Hash containing some configuration for any behavior
+   */
   var Behavior = Base.extend({
-    constructor: function() {
-
+    constructor: function(config) {
+      this._config = config;
     },
 
+    /**
+     * Call this to make this Behavior ready to work
+     */
     setup: function(ship, game) {
-      console.log("SET", arguments);
       this._ship = ship;
       this._game = game;
       this.started = true;
       this._onRegister();
     },
 
-    // called when behavior is being registerd
+    /**
+     * called when behavior is being registered
+     */
     _onRegister: function() {
 
     },
 
-    // called when behavior is being unregisterd (or ship is destroyed)
+    /**
+     * called when behavior is being unregisterd (or ship is destroyed)
+     */
     _onDestroy: function() {
 
     },
 
-    // should be overriden by real behavior that does something
+    /**
+     * should be overriden by real behavior that does something
+     * @param {Number} [frameDuration] How many microseconds have passed since last frame
+     */
     loop: function(frameDuration) {
     }
   });
--- assets/javascripts/ai/shoot_on_sight_behavior.js
+++ assets/javascripts/ai/shoot_on_sight_behavior.js
@@ -3,9 +3,14 @@
 ;(function(win) {
   "use strict";
 
+  /**
+   * Simple behavior that shoots on every foe that is in range of this ship cannon
+   * @param {Array} [config.friends] Array of SpaceShips on which this ship will NOT shoot
+   */
   var ShootOnSightBehavior = Behavior.extend({
     // Setup proximity trigger, and shoot at the targets
     _onRegister: function() {
+      this._friends = this._config.friends;
     },
 
     // shoots if something is in sight
@@ -19,7 +24,7 @@
         }
       } else {
         this._game.ships.forEach(function(ship) {
-          if (!this._currentTarget && this._ship !== ship && this.inRange(ship)) {
+          if (!this._currentTarget && this._ship !== ship && this.inRange(ship) && this._friends.indexOf(ship) === -1) {
             this._currentTarget = ship;
           }
         }.bind(this));
